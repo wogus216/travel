91fa8b5925ea2303eae274b43dd62ca8a9004614
sancho <effort@salt-mine.io>
2022-10-31 23:23:47 +0900

    1차 완성

--- landingpage/src/FullpageScroll.js
+++ landingpage/src/FullpageScroll.js
@@ -4,7 +4,7 @@ import {
   FullpageSection,
   FullPageSections,
 } from '@ap.cx/react-fullpage';
-import React from 'react';
+
 import './FullpageScroll.css';
 
 const FullpageScroll = () => {
@@ -14,34 +14,45 @@ const FullpageScroll = () => {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    color: 'white',
+    fontSize: '20px',
+    lineHeight: '2',
+    animation: 'fadeout 3s',
   };
+
   return (
     <Fullpage>
       <FullpageNavigation />
       <FullPageSections>
-        <FullpageSection style={SectionStyle}>
+        <FullpageSection className="first" style={SectionStyle}>
           <section>
-            <div>고등학교때 친구들과 한 약속 기억나시나요??</div>
-            <br />
-            <div>저는 잊고 지내다...친구한명이 트리거를 해줬어요</div>
-            <br />
-            <div>그래서 10년만에 지키려고 떠나요 !!</div>
+            <div>고등학교 시절</div>
+            <div>약속 기억나세요 ??</div>
+            <div>저는 잊고 지냈지만..</div>
+            <div>더는 후회하기 싫어서</div>
+            <div>약속을 지키러 갑니다 !!</div>
           </section>
         </FullpageSection>
-        <FullpageSection style={SectionStyle}>
+        <FullpageSection className="second" style={SectionStyle}>
           <section>
             <div>그래서 어디로 가냐고요 ??</div>
+
             <div>바로 말하면 재미없잖아요ㅋㅋ</div>
+
             <div>스크롤 하나만 더 내려주세요</div>
           </section>
         </FullpageSection>
-        <FullpageSection style={SectionStyle}>
+        <FullpageSection className="third" style={SectionStyle}>
           <div className="flip">
             <div className="card">
               {/* 카드 앞면  */}
-              <div className="front">클릭 해주세요</div>
+              <div className="front"></div>
               {/* 카드 뒷면 */}
-              <div className="back">만들어야지</div>
+              <div className="back"></div>
+            </div>
+            <div className="back-text">
+              <div>미국 횡단을 갑니다.</div>
+              <div>90일동안 !!</div>
             </div>
           </div>
         </FullpageSection>
